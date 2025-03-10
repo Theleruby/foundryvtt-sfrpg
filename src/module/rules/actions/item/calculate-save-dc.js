ff31530bfabbdf1444e8cf98e23840873f497a85
Ruby Dennington <theleruby@gmail.com>
2025-03-10 00:26:03 +0000

    [Hacks] Add a hook which fires during calculation of an item's save DC

--- src/module/rules/actions/item/calculate-save-dc.js
+++ src/module/rules/actions/item/calculate-save-dc.js
@@ -53,6 +53,10 @@ export default function(engine) {
                     }
                 }
 
+                let formula = { dcFormula: dcFormula };
+                Hooks.callAll("calculateSaveDC", {actor: actor, item: item, formula: formula});
+                dcFormula = formula.dcFormula;
+
                 let computedSave = false;
 
                 if (dcFormula) {
